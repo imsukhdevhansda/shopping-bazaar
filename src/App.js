b8d6718be85cca53815efe81590c119791e30838
mohitd427 <mohitdon0013@gmail.com>
2022-11-11 00:53:06 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar.jsx"
 
 function App() {
   return <div className="App">
+    <Navbar/>
+    <Footer/>
   </div>;
 }
 
--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
+
+ import { Provider } from "react-redux";
 import { store } from "./Redux/store";
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
+    
+    <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-    {/* </BrowserRouter> */}
+    </BrowserRouter>
+   
   </React.StrictMode>
 );
 
