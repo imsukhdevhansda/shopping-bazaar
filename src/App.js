1a48c6b83b41e5f3847476d52e91a12196712462
Sukhdev Hansda <sukhdevhansda8@gmail.com>
2022-11-13 15:37:27 +0530

    Single product page done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 // import Navbar from "./Components/Navbar";
 // import Home from "./Pages/Home";
 import Men from "./Pages/Men"
+import SingleProduct from "./Pages/SingleProductP";
 function App() {
   return <div className="App">
     {/* <Navbar/> */}
     {/* <Footer/> */}
     {/* <Home/> */}
     {/* <Men/> */}
+    <SingleProduct/>
   </div>;
 
 }
